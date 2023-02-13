bd7d73b2961d353ec69abc674f05fdce0974ac52
Hans Christian Reinl <info@drublic.de>
2023-02-13 09:49:26 +0100

    Add more text to about me page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import styles from "./index.module.css";
 import Footer from "../lib/Footer";
 import HeaderWebsite from "../lib/HeaderWebsite";
+import Link from "next/link";
 
 export default () => {
   return (
@@ -20,7 +21,19 @@ export default () => {
         <h1>About ResumerX</h1>
 
         <section className={styles.sections} style={{ textAlign: "left" }}>
-          …
+          <p>
+            My name is Hans. You can read more about me{" "}
+            <a href="https://drublic.de/">on my website</a>.
+          </p>
+          <p>I do this on my own just to play around with the OpenAI API.</p>
+          <p>
+            This page or the service are not connected to any recruiting
+            business at all.
+          </p>
+          <p>
+            Your data is not stored with ResumerX. See{" "}
+            <Link href="/my-data">What happens to the data that I enter?</Link>
+          </p>
         </section>
       </main>
 
